b278112e5ac84ecbf83c844efe18ca90c8d5a806
YokelMan <vedantnjadhav@gmail.com>
2022-03-09 18:39:07 +0530

    starting commit

---
+++ GodSimulator.py
@@ -0,0 +1,60 @@
+# project author: YokelMan
+# project start date: 8th March, 2022
+
+import os
+from datetime import datetime
+
+saveGameMetaData = [["sample 1", 1], ["sample 2", 2]] # template: title, saveTimeInEpoch
+saveGameData = [] # template: stuff
+
+def introScene():
+    os.system("cls")
+    print("(Enter any key to continue the dialog.)")
+    print("Once upon a time the Gods reigned supreme. The people were afraid of them, they felt their existence and worshipped them.")
+    input()
+    print("But now, times have changed. The Gods have slackened and as a result people have started to become infidels.")
+    input()
+    print("You might be wondering what you are doing here; your confusion is valid.")
+    input()
+    print("You were once a mortal, but you proved yourself to be wild yet elegant, bold yet noble, fierce yet kind.")
+    input()
+    print("I will not reveal my identity to you, but I will tell you your objective:")
+    input()
+    print("It is up to you now to restore the status of the Gods. You may be a mortal but you will be able to accomplish this task. Go, human.")
+    input()
+    os.system("cls")
+
+def start():
+    print("Welcome to God Simulator!")
+    choice = int(input("Press 1 to load a previously saved game or press 2 to start a new game! Enter anything else if you want to exit!\n"))
+    if (choice != 1 and choice != 2):
+        print("Bye bye!")
+        exit()
+    elif (choice == 1):
+        if len(saveGameMetaData):
+            os.system("cls")
+            print("Which save game do you want to load?")
+            for x in saveGameMetaData:
+                date = datetime.utcfromtimestamp(x[1]).strftime('%d-%m-%Y')
+                time = datetime.utcfromtimestamp(x[1]).strftime('%H:%M:%S')
+                print(f"Title: {x[0]}\nSave date: {date}\nSave time: {time}\n")
+            print("(Date format is dd/mm/yyyy and time format is hour/minute/second. Time is in UTC.)")
+            userInput = input("Enter the title of your save game to load it: ")
+            game(userInput)
+        else:
+            print("You don't have any save game!")
+            input("Enter any key to continue...\n")
+            choice = 2
+            os.system("cls")
+    if (choice == 2):
+        print("Hey there! This is a game about training to become the best god in the Universe!")
+        print("You will have to fight enemies, powerful bosses and level up to become a true God.")
+        input("Ready to start? Enter any key to continue.\n")
+        introScene()
+
+def game(choice = None):
+    # stuff will be added later, need to check if title of save game matches or not
+    pass
+
+if __name__ == "__main__":
+    start()
